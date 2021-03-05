b1a74c4a227bf1f474c03197aa27e7b710511682
Adri11n <adriaanvanvliet2005@gmail.com>
2021-03-05 23:52:19 +0100

    made the file working in cli mode

--- autoremove.bat
+++ autoremove.bat
@@ -1,6 +1,6 @@
 @echo off
-pip freeze > requirements.txt
-pip uninstall -r requirements.txt
+python -m pip freeze > requirements.txt
+IF %1% == y (python -m pip uninstall -r requirements.txt -y) ELSE python -m pip uninstall -r requirements.txt
 del requirements.txt
 cls
-pip list
+python -m pip list
--- autoremove.sh
+++ autoremove.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
-pip freeze > requirements.txt
-pip uninstall -r requirements.txt
-rm requirements.txt
-clear
-pip list
+echo "python3 -m pip3 freeze > requirements.txt"
+if [ $1 = "y" ]; then
+	echo "python3 -m pip3 uninstall -r requirements.txt -y"
+else
+	echo "python3 -m pip3 uninstall -r requirements.txt"
+fi
+echo "rm requirements.txt"
+echo "clear"
+echo "python3 -m pip3 list"
