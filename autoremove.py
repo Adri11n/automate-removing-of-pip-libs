2c047797b08c10aff48222e29cedbddb4235c87d
Adri11n <adriaanvanvliet2005@gmail.com>
2021-03-06 10:57:22 +0100

    final version addet linux and mac support

--- autoremove.py
+++ autoremove.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import os, platform
-def rwp(filename="autoremove.bat", remove=False, output=True): #stands for remove from pip
+def rpl(filename="autoremove.bat", remove=False, output=True): #stands for remove pip libs
     if platform.system() == "Windows":
         if os.path.isfile(filename):
             if not remove and output:
@@ -21,6 +21,24 @@ def rwp(filename="autoremove.bat", remove=False, output=True): #stands for remov
     elif platform.system() == "Linux":
         if filename == "autoremove.bat":
             filename = "autoremove.sh"
+        if os.path.isfile(filename):
+            os.system(f"chmod +x {filename}")
+            if not remove and output:
+                os.system(filename)
+                print("\nsuccesfully uninstalled all pip libs")
+            elif remove and output:
+                os.system(filename + " y")
+                print("\nsuccesfully uninstalled all pip libs without prompting for yes/no")
+            elif remove and not output:
+                os.system(filename + " y > /dev/nul")
+                return ("\nsuccesfully uninstalled all pip libs without prompting for yes/no and without output")
+            elif not remove and not output:
+                print("\nERROR\nwithout output of the command you cannot response to the command\ninvalide option")
+            else:
+                print("\nERROR\n")
+    elif platform.system() == "Darwin":
+        if filename == "autoremove.bat":
+            filename = "autoremove.apple"
         if os.path.isfile(filename):
             os.system(f"chmod +x {filename}")
             if not remove and output:
