0bd7e00e4955407c50093b207e36beb5c0420392
Adri11n <adriaanvanvliet2005@gmail.com>
2021-03-06 01:01:07 +0100

    addet \n

--- autoremove.py
+++ autoremove.py
@@ -5,13 +5,13 @@ def rwp(filename="autoremove.bat", remove=False, output=True): #stands for remov
         if os.path.isfile(filename):
             if not remove and output:
                 os.system(filename + " n")
-                print("succesfully uninstalled all pip libs")
+                print("\nsuccesfully uninstalled all pip libs")
             elif remove and output:
                 os.system(filename + " y")
-                print("succesfully uninstalled all pip libs without prompting for yes/no")
+                print("\nsuccesfully uninstalled all pip libs without prompting for yes/no")
             elif remove and not output:
                 os.system(filename + " y > nul")
-                return ("succesfully uninstalled all pip libs without prompting for yes/no and without output")
+                return ("\nsuccesfully uninstalled all pip libs without prompting for yes/no and without output")
             elif not remove and not output:
                 print("\nERROR\nwithout output of the command you cannot response to the command\ninvalide option")
             else:
@@ -25,13 +25,13 @@ def rwp(filename="autoremove.bat", remove=False, output=True): #stands for remov
             os.system(f"chmod +x {filename}")
             if not remove and output:
                 os.system(filename)
-                print("succesfully uninstalled all pip libs")
+                print("\nsuccesfully uninstalled all pip libs")
             elif remove and output:
                 os.system(filename + " y")
-                print("succesfully uninstalled all pip libs without prompting for yes/no")
+                print("\nsuccesfully uninstalled all pip libs without prompting for yes/no")
             elif remove and not output:
                 os.system(filename + " y > /dev/nul")
-                return ("succesfully uninstalled all pip libs without prompting for yes/no and without output")
+                return ("\nsuccesfully uninstalled all pip libs without prompting for yes/no and without output")
             elif not remove and not output:
                 print("\nERROR\nwithout output of the command you cannot response to the command\ninvalide option")
             else:
