011579b876b0fd1699667b7b8a223b0a1e79446c
Adri11n <76658955+Adri11n@users.noreply.github.com>
2021-03-06 23:40:08 +0100

    Update autoremove.bat

--- autoremove.bat
+++ autoremove.bat
@@ -1,7 +1,6 @@
 @echo off
 python -m pip freeze > requirements.txt
-IF %1%==y python -m pip uninstall -r requirements.txt -y
-IF %1%==n python -m pip uninstall -r requirements.txt
+python -m pip uninstall -r requirements.txt
 del requirements.txt
 cls
 python -m pip list
