26e875aa71b95ea383e1556a01a169dc00ae7ee7
Adri11n <76658955+Adri11n@users.noreply.github.com>
2021-03-06 23:39:14 +0100

    Update autoremove.sh

--- autoremove.sh
+++ autoremove.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/bash
 python3 -m pip3 freeze > requirements.txt
-if [ $1 = "y" ]; then
-	python3 -m pip3 uninstall -r requirements.txt -y
-else
-	python3 -m pip3 uninstall -r requirements.txt
-fi
+python3 -m pip3 uninstall -r requirements.txt
 rm requirements.txt
 clear
 python3 -m pip3 list
