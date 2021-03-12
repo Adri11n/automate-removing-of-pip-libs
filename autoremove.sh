c2614b4768ac86c384e9c559f98d24feb7ead4a8
Adri11n <76658955+Adri11n@users.noreply.github.com>
2021-03-12 23:03:34 +0100

    Update autoremove.sh

--- autoremove.sh
+++ autoremove.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-python3 -m pip3 freeze > requirements.txt
-python3 -m pip3 uninstall -r requirements.txt
+python3 -m pip freeze > requirements.txt
+python3 -m pip uninstall -r requirements.txt
 rm requirements.txt
 clear
-python3 -m pip3 list
+python3 -m pip list
